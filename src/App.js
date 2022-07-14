3ae1eeccff779a0997228bac1e14060e6f6821f8
Mei Deng <meideng06@gmail.com>
2022-07-14 00:32:51 -0400

    added React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default function App() {
+  return ""
 }
-
-export default App;
