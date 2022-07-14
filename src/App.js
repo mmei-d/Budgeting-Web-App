be620a22b4cb7f62f90d0b21083d5955b376fb89
Mei Deng <meideng06@gmail.com>
2022-07-14 15:13:46 -0400

    added header with buttons

--- src/App.js
+++ src/App.js
@@ -1,3 +1,18 @@
+import Container from 'react-bootstrap/Container'
+import { Stack, Button } from 'react-bootstrap'
+
 export default function App() {
-  return ""
+  return(
+    // 'my-4' gives spacing at top of container
+    <Container className='my-4'>
+      {/* stack is a bootstrap flexbox container */}
+      {/* 'mb-4' gives bottom margin more space */}
+      <Stack direction="horizontal" gap="2" className="mb-4">
+        {/* 'me-auto' aligns heading to far left */}
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="primary">Add Budget</Button>
+        <Button variant="outline-primary">Add Expense</Button>
+      </Stack>
+    </Container>
+  )
 }
