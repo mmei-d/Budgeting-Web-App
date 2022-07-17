1e5958ebc93431f2d992d3e77d968014c42a5fee
Mei Deng <meideng06@gmail.com>
2022-07-17 02:05:23 -0400

    move expenses to uncategorized when budget is deleted

--- src/contexts/BudgetsContext.js
+++ src/contexts/BudgetsContext.js
@@ -39,6 +39,13 @@ export const BudgetsProvider = ({children}) => {
     }
 
     function deleteBudget({id}){
+        setExpenses(prevExpenses => {
+            return prevExpenses.map(expense => {
+                if(expense.budgetId !== id) return expense
+                return {...expense, budgetId: UNCATEGORIZED_BUDGET_ID}
+            })
+        })
+
         setBudgets(prevBudgets => {
             return prevBudgets.filter(budget => budget.id !== id)
         })
