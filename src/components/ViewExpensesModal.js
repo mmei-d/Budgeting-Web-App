5636b11489ec93ffc8a197e7019bf9c6886aafeb
Mei Deng <meideng06@gmail.com>
2022-07-17 01:59:52 -0400

    list expense items in view expense modal

--- src/components/ViewExpensesModal.js
+++ src/components/ViewExpensesModal.js
@@ -1,10 +1,13 @@
 import { Button, Modal, Stack } from 'react-bootstrap'
 import { UNCATEGORIZED_BUDGET_ID, useBudgets } from '../contexts/BudgetsContext'
+import { currencyFormatter } from '../utils'
 
 export default function ViewExpensesModal({budgetId, handleClose}) {
     const { getBudgetExpenses, budgets, deleteBudget, deleteExpense } = useBudgets() 
 
     const budget = UNCATEGORIZED_BUDGET_ID === budgetId ? {name: 'Uncategorized', id: UNCATEGORIZED_BUDGET_ID} : budgets.find(b => b.id === budgetId)
+
+    const expenses = getBudgetExpenses(budgetId)
 
     return(
         <Modal show={budgetId != null} onHide={handleClose}>
@@ -22,7 +25,15 @@ export default function ViewExpensesModal({budgetId, handleClose}) {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                
+                <Stack direction='vertical' gap='3'>
+                    {expenses.map(expense => (
+                        <Stack direction='horizontal' gap='2' key={expense.id}>
+                            <div className='me-auto fs-4'>{expense.description}</div>
+                            <div className='fs-5'>{currencyFormatter.format(expense.amount)}</div>
+                            <Button size='sm' variant='outline-danger'>&times;</Button>
+                        </Stack>
+                    ))}
+                </Stack>
             </Modal.Body>
 
         </Modal>
